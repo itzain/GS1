550812f7797ec98325e4d0a5a71af2c04726045e
KnowldgeBase <49622102+KnowledgeBI@users.noreply.github.com>
2022-11-24 22:43:23 +0200

    new

--- barcode_custom/__manifest__.py
+++ barcode_custom/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "barcode_custom",
+    'name': "Medical Barcode",
 
     'summary': """
         Smart solution for medical barcode""",
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Warehouse',
-    'version': '0.1',
+    'version': '13.0.0.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base','stock'],
